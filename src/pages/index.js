580494d6e621ef1f51c855f63dbb0d27694eca48
Jaseem vk <muhammedjaseemvk96@gmail.com>
2023-04-15 15:11:02 +0530

    added form to index.have some submission issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,20 @@
 import { MdOutlineQrCode } from 'react-icons/md';
 import { MdArrowForward } from 'react-icons/md'
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Home() {
+  const [admissionNo, setadmissionNo] = useState('')
+
+  async function submitHandler(e) {
+    e.preventDefault();
+    console.log(admissionNo)
+  }
+
+  async function scanMe () {
+    console.log('hello')
+  }
+
   return (
     <div className='h-screen bg-[#213458] relative text-black'>
       {/* Logo */}
@@ -12,10 +24,10 @@ export default function Home() {
 
 
       {/* Bottom Area */}
-      <div className="bg-white absolute bottom-0 left-0 w-full py-8 rounded-t-2xl px-10 flex flex-col gap-2">
+      <form onSubmit={submitHandler} className="bg-white absolute bottom-0 left-0 w-full py-8 rounded-t-2xl px-10 flex flex-col gap-2">
             <div>
               <div className="text-md">Admission No</div>
-              <input type="text" className="bg-[#7E7A7A] p-2 rounded-md w-full" placeholder="Admission No" />
+              <input type="text" className="bg-[#7E7A7A] p-2 rounded-md w-full" placeholder="Admission No" required={true} value={admissionNo} onChange={e => setadmissionNo(e.target.value)} />
             </div>
             
 
@@ -28,15 +40,15 @@ export default function Home() {
 
             {/* Buttons */}
             <div>
-              <button className="btn btn-primary">Scan
+              <button type='button' onClick={scanMe} className="btn btn-primary">Scan
                 <MdOutlineQrCode />
               </button>
-              <button className='btn btn-secondary'>
+              <button type='submit' className='btn btn-secondary'>
                 Submit
                 <MdArrowForward />
               </button>
             </div>
-      </div>
+      </form>
     </div>
 
   )
---
+++ src/pages/validpass.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Image from 'next/image'
+import { MdArrowForward } from 'react-icons/md'
+import { MdOutlineQrCode } from 'react-icons/md'
+
+export default function validpass() {
+  return (
+    <div className='h-screen bg-[#213458] relative text-black'>
+      {/* Logo */}
+      <div className='w-full h-3/4 flex items-center'>
+        <Image src="/pass-validator-logo.png" alt="logo" width='250' height='250' className='mx-auto' />
+      </div>
+
+
+      {/* Bottom Area */}
+      <div className="bg-white absolute bottom-0 left-0 w-full py-8 rounded-t-2xl px-10 flex flex-col gap-2">
+        <div>
+          <div className="text-md">Valid bus pass</div>
+          
+          {/* <input type="text" className="bg-[#7E7A7A] p-2 rounded-md w-full" placeholder="Admission No" /> */}
+        </div>
+
+
+        {/* OR Bar */}
+        <div className="flex flex-row justify-between items-center">
+          <div className='w-1/2 h-[1px] bg-black'></div>
+          <p className="mx-2">OR</p>
+          <div className='w-1/2 h-[1px] bg-black'></div>
+        </div>
+
+        {/* Buttons */}
+        <div>
+          <button className="btn btn-primary">Scan
+            <MdOutlineQrCode />
+          </button>
+          <button className='btn btn-secondary'>
+            Submit
+            <MdArrowForward />
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
