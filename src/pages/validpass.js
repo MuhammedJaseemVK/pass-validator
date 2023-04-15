b913f031c8d401ea17429e90cfc09e40d30dc2c4
Jaseem vk <muhammedjaseemvk96@gmail.com>
2023-04-15 16:35:03 +0530

    created validpass page

--- src/pages/validpass.js
+++ src/pages/validpass.js
@@ -1,42 +1,64 @@
 import React from 'react'
 import Image from 'next/image'
-import { MdArrowForward } from 'react-icons/md'
-import { MdOutlineQrCode } from 'react-icons/md'
+import { AiFillCheckCircle } from 'react-icons/ai'
+import { AiOutlineClose } from 'react-icons/ai'
+import { ImHappy2 } from 'react-icons/im'
+import Link from 'next/link'
+
 
 export default function validpass() {
+
+
   return (
     <div className='h-screen bg-[#213458] relative text-black'>
       {/* Logo */}
-      <div className='w-full h-3/4 flex items-center'>
+      <div className='w-full h-1/2 flex items-center'>
         <Image src="/pass-validator-logo.png" alt="logo" width='250' height='250' className='mx-auto' />
       </div>
 
 
       {/* Bottom Area */}
       <div className="bg-white absolute bottom-0 left-0 w-full py-8 rounded-t-2xl px-10 flex flex-col gap-2">
-        <div>
-          <div className="text-md">Valid bus pass</div>
-          
-          {/* <input type="text" className="bg-[#7E7A7A] p-2 rounded-md w-full" placeholder="Admission No" /> */}
+        <div className='w-full flex items-center justify-center'>
+          <div className="text-2xl">Valid bus pass</div>
+          <AiFillCheckCircle className='text-green-500' />
         </div>
 
 
-        {/* OR Bar */}
-        <div className="flex flex-row justify-between items-center">
-          <div className='w-1/2 h-[1px] bg-black'></div>
-          <p className="mx-2">OR</p>
-          <div className='w-1/2 h-[1px] bg-black'></div>
+
+        <div className='text-xl'>
+          <div className='flex flex-row justify-between'>
+            <p>Student name:</p>
+            <p>jaseem</p>
+          </div>
+          <div className='flex flex-row justify-between'>
+            <p>year of join:</p>
+            <p>jaseem</p>
+          </div>
+          <div className='flex flex-row justify-between'>
+            <p>valid till:</p>
+            <p>jaseem</p>
+          </div>
+          <div className='flex flex-row justify-between'>
+            <p>bus stop:</p>
+            <p>jaseem</p>
+          </div>
+          
+          
+        </div>
+
+        <div>
+          <ImHappy2 className='text-8xl text-green-500 mx-auto'/>
         </div>
 
         {/* Buttons */}
         <div>
-          <button className="btn btn-primary">Scan
-            <MdOutlineQrCode />
-          </button>
-          <button className='btn btn-secondary'>
-            Submit
-            <MdArrowForward />
-          </button>
+          <Link href="/"> <button className="btn btn-secondary">Close
+            <AiOutlineClose />
+            </button>
+          </Link>
+          
+
         </div>
       </div>
     </div>
